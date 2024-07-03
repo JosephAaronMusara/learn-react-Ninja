a4871286d502b0667d5378dc249f6d9c563cc502
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-03 16:10:54 +0200

    The useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -14,6 +14,15 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
+    useEffect(()=>{
+        //another built-in hook
+        // This runs at initial runtime or whenever new data is rendered
+        //UseCases: Fetch Data, Communicating with other services to notify about such changes
+
+        console.log('useEffect has been invoked');
+
+    });
+
     // const [name,setName] = useState('Aaron');
     // const [age,setAge] = useState(21)
 
@@ -25,7 +34,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author ==='mario')}  title="Mario's Blogs"/>
+            {/* <BlogList blogs={blogs.filter((blog)=>blog.author ==='mario')}  title="Mario's Blogs" handleDelete={handleDelete}/> */}
 
             {/* <h2>Home Page</h2>
             <p>{name} is {age} years old!</p>
