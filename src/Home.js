2c7cc9544553797b23be90d0543f2fe561d4ccd0
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-03 16:49:58 +0200

    The useEffect Dependencies, ndaisa zvangu chiri random, to trigger it several times

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs,setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: 'My First blog Post', body: 'Demo body ......', author: 'Joseph', id: 1 },
+        { title: 'Welcome party!', body: ' Demo body2 ......', author: 'Aaron', id: 2 },
+        { title: 'Web dev top tips', body: 'Demo body 3 ......', author: 'Joseph', id: 3 }
     ]);
 
     const handleDelete = (id) => {
