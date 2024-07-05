16e7ec2bd6aa3960e40e762e127dfbf51b2f4c49
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-05 11:05:32 +0200

    React Router-Exact path

---
+++ src/Create.js
@@ -0,0 +1,11 @@
+const Create = () => {
+    return (
+        <div className="create">
+            <h2>
+                Create a new post
+            </h2>
+        </div>
+    );
+}
+ 
+export default Create;
