76c1a7cb5febf19f88e8b12e3efad8ce1439662f
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-05 09:41:47 +0200

    More Error Handling, With conditional template rendering

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,26 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error,setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+            if(!res.ok){
+                throw Error('Data fetch from the resource failed');
+            }
           return res.json();
         })
         .then((data) => {
-          console.log(data);
           setBlogs(data);
           setIsLoading(false);
+          setError(null);
         })
         .catch(err=>{
+            setIsLoading(false);
             console.log(err.message);
+            setError(err.message);
         });
     }, 1000); //added a delay to see the effect. Never do this in practice
   }, []);
@@ -47,6 +53,7 @@ const Home = () => {
 
   return (
     <div className="home">
+        {error && <div>{error}</div>}
       {isLoading && <div>Loading............</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
 
