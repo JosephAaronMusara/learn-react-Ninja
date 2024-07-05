95f43423358b2eba975598e00a1e497a4657039c
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-05 09:46:34 +0200

    Removed all commented code

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,15 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [error,setError] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
-            if(!res.ok){
-                throw Error('Data fetch from the resource failed');
-            }
+          if (!res.ok) {
+            throw Error("Data fetch from the resource failed");
+          }
           return res.json();
         })
         .then((data) => {
@@ -20,53 +20,19 @@ const Home = () => {
           setIsLoading(false);
           setError(null);
         })
-        .catch(err=>{
-            setIsLoading(false);
-            console.log(err.message);
-            setError(err.message);
+        .catch((err) => {
+          setIsLoading(false);
+          console.log(err.message);
+          setError(err.message);
         });
     }, 1000); //added a delay to see the effect. Never do this in practice
   }, []);
 
-  // const handleDelete = (id) => {
-  //     const newBlogs = blogs.filter(blog => blog.id !== id);
-  //     setBlogs(newBlogs);
-  // }
-
-  //const [name,setName] = useState('Aaron');
-  //const [age,setAge] = useState(21)
-
-  // const handleClick =() =>{
-  //     setName('Tariroyashe');
-  //     setAge(17);
-  // }
-
-  // useEffect(()=>{
-  //     //another built-in hook
-  //     // This runs at initial runtime or whenever new data is rendered
-  //     //UseCases: Fetch Data, Communicating with other services to notify about such changes
-
-  //     console.log('useEffect has been invoked');
-
-  // },[age] //We can run it conditionally by giving it a secont argument as an array, which we can implement sensibly through states
-  // );
-
   return (
     <div className="home">
-        {error && <div>{error}</div>}
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading............</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
-
-      {/* <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> */}
-      {/* <div className='cherandom'>
-                <p>The number is {age}</p>
-                <button onClick={()=>setAge(Math.random)}>Change number</button>
-            </div> */}
-      {/* <BlogList blogs={blogs.filter((blog)=>blog.author ==='mario')}  title="Mario's Blogs" handleDelete={handleDelete}/> */}
-
-      {/* <h2>Home Page</h2>
-            <p>{name} is {age} years old!</p>
-            <button onClick={handleClick}>Click Me</button> */}
     </div>
   );
 };
