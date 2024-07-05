1d51dbcbac5e36d8108b4c094681cdbd29863ecd
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-05 11:27:11 +0200

    Abort Error Handling And Cleaning

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,8 +5,9 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error("Data fetch from the resource failed");
@@ -19,11 +20,14 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          setIsLoading(false);
-          console.log(err.message);
-          setError(err.message);
+          if (!err.name === "AbortError") {
+            setIsLoading(false);
+            console.log(err.message);
+            setError(err.message);
+          }
         });
     }, 1000); //added a delay to see the effect. Never do this in practice
+    return abortCont.abort();
   }, [url]);
 
   return { data, isLoading, error };
