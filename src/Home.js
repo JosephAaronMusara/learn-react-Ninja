cc6ed323047e609167b2fe175b85cc5524eabcd0
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-05 10:35:29 +0200

    Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
-import BlogList from "./BlogList";
+import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Data fetch from the resource failed");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsLoading(false);
-          console.log(err.message);
-          setError(err.message);
-        });
-    }, 1000); //added a delay to see the effect. Never do this in practice
-  }, []);
-
+    const {data:blogs, isLoading,error} = useFetch('http://localhost:8000/blogs');
   return (
     <div className="home">
       {error && <div>{error}</div>}
---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useEffect,useState } from 'react';
+const useFetch = (url) => {
+    
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+          fetch(url)
+            .then((res) => {
+              if (!res.ok) {
+                throw Error("Data fetch from the resource failed");
+              }
+              return res.json();
+            })
+            .then((data) => {
+              setData(data);
+              setIsLoading(false);
+              setError(null);
+            })
+            .catch((err) => {
+              setIsLoading(false);
+              console.log(err.message);
+              setError(err.message);
+            });
+        }, 1000); //added a delay to see the effect. Never do this in practice
+      }, [url]);
+
+      return {data,isLoading,error};
+
+}
+
+export default useFetch;
