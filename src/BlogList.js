d066aabdbccb9e33c667f13b2a9588d78ceea9d0
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-05 10:55:32 +0200

    React Routes

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,16 @@
-const BlogList = ({blogs,title}) => {
-    return (
-        <div className="blog-list">
-            <h2>{title}</h2>
-            {blogs.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Writen by : {blog.author}</p>
-                    {/* <button onClick={()=>handleDelete(blog.id)}>Delete Post</button> */}
-                </div>
-            ))}
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Writen by : {blog.author}</p>
+          {/* <button onClick={()=>handleDelete(blog.id)}>Delete Post</button> */}
         </div>
-    );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
-import BlogList from './BlogList';
-import useFetch from './useFetch';
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const {data:blogs, isLoading,error} = useFetch('http://localhost:8000/blogs');
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
       {error && <div>{error}</div>}
