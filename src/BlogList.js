583536f9adabdf4317f8ea9fc8dd6c9368ea374a
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-05 12:02:53 +0200

    Navigation using Url parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
+          <Link to = {`/blogs/${blog.id}`}>
           <h2>{blog.title}</h2>
           <p>Writen by : {blog.author}</p>
-          {/* <button onClick={()=>handleDelete(blog.id)}>Delete Post</button> */}
+          </Link>
         </div>
       ))}
     </div>
